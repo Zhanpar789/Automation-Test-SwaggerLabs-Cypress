c2fad759547140399b90767d586206f881b8b7ab
Zhanpar <zhanfar.isman@gmail.com>
2025-03-31 15:47:02 +0700

    feat: :sparkles: Add LoginPage

---
+++ swager-labs-demo/src/pages/LoginPage.js
@@ -0,0 +1,38 @@
+class LoginPage {
+    urlLoginPage(){
+         cy.visit('https://www.saucedemo.com/');
+    }
+    get labeLoginPage(){
+        return cy.get('[class="login_logo"]').contains('Swag Labs');
+    }
+    verifyLabelLoginPage(){
+        this.labeLoginPage.should('be.visible');
+    }
+
+    get usernameField(){
+        return cy.get('[data-test="username"]');
+    }
+    clickUsernameField(){
+        this.usernameField.click();
+    }
+    fillUsernameField(){
+        this.usernameField.type('standard_user');
+    }
+
+    get passwordField(){
+        return cy.get('[data-test="password"]');
+    }
+    clickPasswordField(){
+        this.passwordField.click();
+    }
+    fillPasswordField(){
+        this.passwordField.type('secret_sauce');
+    }
+
+    get buttonLogin(){
+        return cy.get('[data-test="login-button"]');
+    }
+    clickButtonLogin(){
+        this.buttonLogin.click();
+    }
+}
