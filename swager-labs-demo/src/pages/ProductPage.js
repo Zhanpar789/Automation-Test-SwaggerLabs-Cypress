71bdb2ca1377a5260a85df1b7f3b1a37117026f2
Zhanpar <zhanfar.isman@gmail.com>
2025-04-03 15:26:46 +0700

    feat: :sparkles: Add All Product

--- swager-labs-demo/src/pages/ProductPage.js
+++ swager-labs-demo/src/pages/ProductPage.js
@@ -9,4 +9,28 @@ export class ProductPage {
         this.tittleProductPage.should('be.visible');
     }
     
+    get productBackpack(){
+        return cy.get('[data-test="add-to-cart-sauce-labs-backpack"]');
+    }
+
+    get productTshirt(){
+        return cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]');
+    }
+
+    get productBikeLight(){
+        return cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]');
+    }
+
+    get productFleeceJacket(){
+        return cy.get('[data-test="add-to-cart-sauce-labs-fleece-jacket"]');
+    }
+
+    get productOnesie(){
+        return cy.get('[data-test="add-to-cart-sauce-labs-onesie"]');
+    }
+
+    get productTshirtRed(){
+        return cy.get('[data-test="add-to-cart-test.allthethings()-t-shirt-(red)"]');
+    }
+
 }
