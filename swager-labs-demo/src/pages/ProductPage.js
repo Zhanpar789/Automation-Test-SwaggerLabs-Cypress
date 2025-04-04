67329b76e97ec606d12d1f8d08c95ff8f22d7996
Zhanpar <zhanfar.isman@gmail.com>
2025-04-04 14:25:12 +0700

    feat: :sparkles: Add Click Add To Cart Product

--- swager-labs-demo/src/pages/ProductPage.js
+++ swager-labs-demo/src/pages/ProductPage.js
@@ -13,24 +13,48 @@ export class ProductPage {
         return cy.get('[data-test="add-to-cart-sauce-labs-backpack"]');
     }
 
+    clickAddToCartProductBackpack(){
+        this.productBackpack.click();
+    }
+
     get productTshirt(){
         return cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]');
+    }
+
+    clickAddToCartProductTshirt(){
+        this.productTshirt.click();
     }
 
     get productBikeLight(){
         return cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]');
     }
 
+    clickAddToCartProductBikeLight(){
+        this.productBikeLight.click();
+    }
+
     get productFleeceJacket(){
         return cy.get('[data-test="add-to-cart-sauce-labs-fleece-jacket"]');
+    }
+
+    clickAddToCartProductFleeceJacket(){
+        this.productFleeceJacket.click();
     }
 
     get productOnesie(){
         return cy.get('[data-test="add-to-cart-sauce-labs-onesie"]');
     }
 
+    clickAddToCartProductOnesie(){
+        this.productOnesie.click();
+    }
+
     get productTshirtRed(){
         return cy.get('[data-test="add-to-cart-test.allthethings()-t-shirt-(red)"]');
+    }
+
+    clickAddToCartProductTshirtRed(){
+        this.productTshirtRed.click();
     }
 
 }
