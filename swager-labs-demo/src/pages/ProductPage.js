040228a34270b0fa792e147fc0808d97ead935bf
Zhanpar <zhanfar.isman@gmail.com>
2025-04-04 19:11:41 +0700

    feat: :sparkles: Add Get Product Removed

--- swager-labs-demo/src/pages/ProductPage.js
+++ swager-labs-demo/src/pages/ProductPage.js
@@ -13,12 +13,20 @@ export class ProductPage {
         return cy.get('[data-test="add-to-cart-sauce-labs-backpack"]');
     }
 
+    get productBackpackRemoved(){
+        return cy.get('[data-test="remove-sauce-labs-backpack"]')
+    }
+
     clickAddToCartProductBackpack(){
         this.productBackpack.click();
     }
 
     get productTshirt(){
         return cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]');
+    }
+
+    get productTshirtRemoved(){
+        return cy.get('[data-test="remove-sauce-labs-bolt-t-shirt"]')
     }
 
     clickAddToCartProductTshirt(){
@@ -29,12 +37,19 @@ export class ProductPage {
         return cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]');
     }
 
+    get productBikeLightRemoved(){
+        return cy.get('[data-test="remove-sauce-labs-bike-light"]');
+    }
+
     clickAddToCartProductBikeLight(){
         this.productBikeLight.click();
     }
 
     get productFleeceJacket(){
         return cy.get('[data-test="add-to-cart-sauce-labs-fleece-jacket"]');
+    }
+    get productFleeceJacketRemoved(){
+        return cy.get('[data-test="remove-sauce-labs-fleece-jacket" ]');
     }
 
     clickAddToCartProductFleeceJacket(){
@@ -45,12 +60,20 @@ export class ProductPage {
         return cy.get('[data-test="add-to-cart-sauce-labs-onesie"]');
     }
 
+    get productOnesieRemoved(){
+        return cy.get('[ddata-test="remove-sauce-labs-onesie"]');
+    }
+
     clickAddToCartProductOnesie(){
         this.productOnesie.click();
     }
 
     get productTshirtRed(){
         return cy.get('[data-test="add-to-cart-test.allthethings()-t-shirt-(red)"]');
+    }
+
+    get productTshirtRedRemoved(){
+        return cy.get('[data-test="remove-test.allthethings()-t-shirt-(red)"]');
     }
 
     clickAddToCartProductTshirtRed(){
