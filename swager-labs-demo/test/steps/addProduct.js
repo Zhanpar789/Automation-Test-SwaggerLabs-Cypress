0b306cf528e296b5ca37e7361bdf9a65c2f09108
Zhanpar <zhanfar.isman@gmail.com>
2025-04-04 19:27:30 +0700

    feat: :sparkles: Add Given User At Product Page

--- swager-labs-demo/test/steps/addProduct.js
+++ swager-labs-demo/test/steps/addProduct.js
@@ -1,7 +1,12 @@
-import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 import { ProductPage } from "../../src/pages/ProductPage";
 
 const productPage = new ProductPage();
+
+Given ('User at Product Page', () => {
+    cy.visit(`${Cypress.env("sauce_demo")}/inventory.html`);
+    cy.wait(1000); 
+})
 
 When ('User Add To Cart Product BackPack', () => {
     productPage.clickAddToCartProductBackpack();
