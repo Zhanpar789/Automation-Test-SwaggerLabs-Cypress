2e67155452b937d30a268033ab04ab8e048a517a
Zhanpar <zhanfar.isman@gmail.com>
2025-04-04 19:22:06 +0700

    feat: :sparkles: Add Steps Then VerifyProductRemoved

--- swager-labs-demo/test/steps/addProduct.js
+++ swager-labs-demo/test/steps/addProduct.js
@@ -7,22 +7,46 @@ When ('User Add To Cart Product BackPack', () => {
     productPage.clickAddToCartProductBackpack();
 });
 
+Then ('User Will See Backpack Add To Cart', () => {
+    productPage.verifyProductBackpackRemoved();
+})
+
 When ('User Add To Cart Product T-Shirt', () => {
     productPage.clickAddToCartProductTshirt();
 });
+
+Then ('User Will See T-Shirt Add To Cart', () => {
+    productPage.verifyProductTshirtRemoved();
+})
 
 When ('User Add To Cart Product Bike Light', () => {
     productPage.clickAddToCartProductBikeLight();
 });
 
+Then ('User Will See Bike Light Add To Cart', () => {
+    productPage.verifyProductBikeLightRemoved();
+})
+
 When ('User Add To Cart Product Fleece Jacket', () => {
     productPage.clickAddToCartProductFleeceJacket();
 });
+
+Then ('User Will See Fleece Jacket Add To Cart', () => {
+    productPage.verifyProductFleeceJacketRemoved();
+})
 
 When ('User Add To Cart Product Onesie', () => {
     productPage.clickAddToCartProductOnesie();
 });
 
+Then ('User Will See Onesie Add To Cart', () => {
+    productPage.verifyProductOnesieRemoved();
+})
+
 When ('User Add To Cart Product T-Shirt Red', () => {
     productPage.clickAddToCartProductTshirtRed();
 });
+
+Then ('User Will See T-Shirt REd Add To Cart', () => {
+    productPage.verifyProductTshirtRedRemoved();
+})
