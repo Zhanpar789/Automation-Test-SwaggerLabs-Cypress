3da554f71da8a1c42a495ee9bdde645dbff0f1bd
Zhanpar <zhanfar.isman@gmail.com>
2025-04-21 21:39:13 +0700

    feat: :sparkles: Add label your cart page

--- swager-labs-demo/src/pages/YourCartPage.js
+++ swager-labs-demo/src/pages/YourCartPage.js
@@ -2,4 +2,11 @@ export class YourCartPage {
     urlYourCartPage(){
         cy.visit('https://www.saucedemo.com/cart.html');
     }
+
+    get labelYourCartPage(){
+        return cy.get('[data-test="title"]').contains('Your Cart');
+    }
+    verifyLabelYourCartPage(){
+        this.labelYourCartPage.should('be.visible');
+    }
 }
