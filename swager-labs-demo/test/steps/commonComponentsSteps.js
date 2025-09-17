b866c418de8724f02ca90ecc8bd56dd31cd59146
Zhanpar <zhanfar.isman@gmail.com>
2025-09-17 22:35:33 +0700

    feat: :sparkles: Add sidebar Steps on commonComponentSteps

--- swager-labs-demo/test/steps/commonComponentsSteps.js
+++ swager-labs-demo/test/steps/commonComponentsSteps.js
@@ -7,3 +7,16 @@ When('User click Burger Button', () => {
     sidebar.clickBurgerButton();
 });
 
+When('User click All Items Sidebar', () => {
+    sidebar.clickAllItemsSidebar();
+});
+When('User click About Sidebar', () => {
+    sidebar.aboutSidebar();
+});
+When('User click Logout Sidebar', () => {
+    sidebar.clickLogoutSidebar();
+});
+When('User click Reset App State Sidebar', () => {
+    sidebar.clickResetAppStateSidebar();
+});
+
