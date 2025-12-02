aff041b8609902e1c1107dfead507ad934ea1439
Zhanpar Isman Andria <91723422+Zhanpar789@users.noreply.github.com>
2025-12-02 21:59:13 +0700

    feat(YourInformation): :sparkles: Add lastNameField (#16)

--- swager-labs-demo/src/pages/YourInformationPage.js
+++ swager-labs-demo/src/pages/YourInformationPage.js
@@ -2,4 +2,7 @@ export class YourInformation {
     get firstNameField(){
         return cy.get('[data-test="firstName"]')
     }
+    get lastNameField(){
+        return cy.get('[data-test="lastName"]')
+    }
 }
